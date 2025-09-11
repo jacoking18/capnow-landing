722d4f831d6b330be712fca77bf09af6bb893cde
jacobosimkin <jsimkin@mail.yu.edu>
2025-09-11 13:17:18 -0400

    first working version - Hello CapNow

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',fontFamily:'ui-sans-serif,system-ui'}}>
+      <h1 style={{fontSize:36}}>Hello CapNow 🚀</h1>
+    </main>
+  );
+}
